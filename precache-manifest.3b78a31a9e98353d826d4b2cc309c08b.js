5861bc0258b7ff6510e6b6935887f1ef8c7fcf09
Siddhant Patil <sidomatic11@gmail.com>
2019-07-06 12:00:29 -0700

    Updates

---
+++ precache-manifest.3b78a31a9e98353d826d4b2cc309c08b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4063e1632701c11d040c3d1001d473c",
+    "url": "/task-timer/index.html"
+  },
+  {
+    "revision": "fd45d7a277694dc60a59",
+    "url": "/task-timer/static/css/main.2a0039a2.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/task-timer/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "fd45d7a277694dc60a59",
+    "url": "/task-timer/static/js/main.071311a2.chunk.js"
+  },
+  {
+    "revision": "b9ccb90dd3a411b3b78f",
+    "url": "/task-timer/static/js/runtime~main.546b7746.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/task-timer/static/media/logo.5d5d9eef.svg"
+  }
+]);
