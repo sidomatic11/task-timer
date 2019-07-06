f3aa424a9c8765ddf444bec234f0a57ac72bc3ba
Siddhant Patil <sidomatic11@gmail.com>
2019-07-06 11:51:59 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -5,20 +5,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Starting the task timer app here. No need to be perfect.</p>
       </header>
+
     </div>
   );
 }
